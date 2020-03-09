651b24508f19f10148c157adb2d61182b9f283c3
Nelson Silva <58569467+2000nrss@users.noreply.github.com>
2020-03-09 21:07:13 +0000

    g

---
+++ Ficha 1/Ficha1.js
@@ -0,0 +1,12 @@
+function calculateFinalGrade(practicalGrade, theoreticalGrade) {
+    var finalGrade = (practicalGrade + theoreticalGrade) / 2;
+    if (finalGrade >= 9.5) {
+        console.log("Aprovado")
+    } else {
+        console.log("Reprovado")
+
+    }
+    var c =
+}
+
+calculateFinalGrade(10, 10)
